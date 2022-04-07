f2f61209ba6eb71c218ffc48f5d0c95fb8018a2a
robotdimension <72067114+robotdimension@users.noreply.github.com>
2022-04-07 11:17:57 +0200

    changed updater

--- inkfin-calculator.php
+++ inkfin-calculator.php
@@ -4,9 +4,9 @@ include_once('updater.php');
 
 /**
  * Plugin Name:       Inkfin Loan Calculator
- * Plugin URI:        https://example.com/plugins/the-basics/
+ * Plugin URI:        https://github.com/justin-netage/inkfin-calculator
  * Description:       Displays calculator on selcted pages using shortcode and attributes.
- * Version:           1.0.1
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Net Age
@@ -18,22 +18,12 @@ include_once('updater.php');
  * Domain Path:       /languages
  */
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'inkfin-calculator', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/justin-netage/inkfin-calculator', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com/justin-netage/inkfin-calculator/main', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/justin-netage/inkfin-calculator', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/justin-netage/inkfin-calculator/archive/refs/heads/main.zip', // the zip url of the GitHub repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '4.0', // which version of WordPress does your plugin require?
-        'tested' => '5.9', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-    );
-    new WP_GitHub_Updater($config);
-}
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'http://example.com/path/to/details.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'inkfin-calculator'
+);
 
 add_shortcode('display_calculator', 'display_calculator_shortcode');
 function display_calculator_shortcode( $atts = [], $content = null) {
